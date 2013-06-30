fd035bb972110734415c6514b8c3a40b138ff0a4
Toshihide Tamura (twics_server) <tamura33363@gmail.com>
2013-06-30 14:57:47 +0900

    シードファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -13,4 +14,4 @@ UnkoPosition.create(latitude: 35.612317, longitude: 139.40335)
 UnkoPosition.create(latitude: 35.618091, longitude: 139.406354)
 UnkoPosition.create(latitude: 35.615283, longitude: 139.413371)
 UnkoPosition.create(latitude: 35.618475, longitude: 35.618475)
-put '唐木田周辺のうんこデータ投入完了'
+puts '唐木田周辺のうんこデータ投入完了'
